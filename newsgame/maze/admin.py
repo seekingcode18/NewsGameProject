407319baa0e8a6d412488417ebb131baea33e91a
DanLedwos <32643853+Ledwos@users.noreply.github.com>
2020-02-07 17:18:12 +0000

    almost working score table

--- newsgame/maze/admin.py
+++ newsgame/maze/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from maze.models import Score
 
 # Register your models here.
+
+admin.site.register(Score)
--- newsgame/maze/models.py
+++ newsgame/maze/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class Score(models.Model):
+    points = models.IntegerField(blank=True)
+    level = models.IntegerField(blank=True)
+    users = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    # def __str__(self):
+    #     return self.points, self.level, self.users
+    
