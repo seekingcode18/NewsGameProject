e11c4397c1f23cf4f926afe595079994151a70c2
DanLedwos <32643853+Ledwos@users.noreply.github.com>
2020-03-03 11:16:49 +0000

    fix initial points

--- newsgame/users/models.py
+++ newsgame/users/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     pass
-    points = models.IntegerField(null=True)
-    level = models.IntegerField(null=True)
+    points = models.IntegerField(default=0, verbose_name=0)
+    level = models.IntegerField(default=0, verbose_name=0)
 
     def __str__(self):
         return self.username
